bc5352aa9c0ebe40b7953251529b623af0df1d8d
caveiramica14 <a63586@alunos.ipb.pt>
2026-09-16 20:03:30 +0100

    Acceleration passo 2

--- Assets/Velocity/Scripts/MoveShell.cs
+++ Assets/Velocity/Scripts/MoveShell.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveShell : MonoBehaviour
 {
-    float speed = 1.0f;
+    public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
